dc4d14235db37b14327722daa98cff064811b5b6
hxg <hiroki.gota@spatialvision.com.au>
2015-06-30 13:26:25 +1000

    git

---
+++ equip_git.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#
+# Ubuntu Equip 
+#  GIT + base
+# https://www.digitalocean.com/community/tutorials/how-to-install-git-on-ubuntu-14-04
+# Licence: MIT
+
+wget --no-check-certificate https://github.com/SpatialVision/ubuntu-equip/raw/master/equip_base.sh && bash equip_base.sh
+
+sudo apt-get install git
